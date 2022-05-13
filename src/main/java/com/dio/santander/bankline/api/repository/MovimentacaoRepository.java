58f9a608e0a738c1286d3f5ff2458033a5982999
DM0R3N0 <dfm84@hotmail.com>
2022-05-13 13:27:44 -0300

    update Controller

--- src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
+++ src/main/java/com/dio/santander/bankline/api/repository/MovimentacaoRepository.java
@@ -8,4 +8,5 @@ import com.dio.santander.bankline.api.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
 	public List<Movimentacao>findByIdConta(Integer idConta);
+	
 }
